fdfc1f0a5814b350d1a5faa9acbf11e845edfc9a
[blanchegithub] <[shaznyblanchetallido@hotmail.com]>
2019-02-12 13:15:02 +0000

    Strings Modification for Practice

--- Strings.c
+++ Strings.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 #include <string.h>
+
 int main() {
-char firstname[] = "Mike";
-char lastname[] = "Child";
-char fullname[50];
-char formalname[50];
+const char* flabby[20], anny[20];
 
-printf("%s\n", firstname);
-printf("%s\n", lastname);
+printf("Insert your first and last name\n");
+scanf("%s %s\n", &flabby, &anny);
 
-char* fullname = malloc(strlen(firstname) + strlen(lastname) + 1);
-memcpy(fullname, &firstname, strlen(firstname) + 1);
-memcpy(fullname + strlen(firstname), &lastname, strlen(lastname) + 1);
-printf(fullname);
+printf("%s\n", flabby);
+printf("%s\n", anny);
+
+//Here the declaration and the definition of the pointer
+//is together with malloc
+char* fatjo = malloc(strlen(flabby) + strlen(anny) + 1);
+memcpy(fatjo, &flabby, strlen(flabby)+1);
+memcpy(fatjo + strlen(flabby), &anny, strlen(anny) + 1);
+
+printf(fatjo);
+printf("\n Address of Flabby variable: %u\n", &flabby);
+printf("\n Flabbies address in HEX: %x\n", &flabby);
 
 return 0;
 }
